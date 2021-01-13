2d0ed4e863317754f2e6389103542704edb3d7bd
minguu <minguu42@gmail.com>
2021-01-13 09:24:20 +0900

    VideoListコンポーネントの作成

---
+++ src/components/VideoList.js
@@ -0,0 +1,64 @@
+import './VideoList.css';
+
+const VideoList = () => {
+    return (
+        <div className="dev-back-c video-list">
+            <div className="video-card">
+                <img className="video-card__thumbnail" src="http://placehold.jp/332x186.png" alt="動画サンプル"/>
+                <h4 className="video-card__title">動画タイトル</h4>
+                <p className="video-card__description">概要ああああああああああああああああああ</p>
+                <button className="video-card__button">今から見る</button>
+            </div>
+            <div className="video-card">
+                <img className="video-card__thumbnail" src="http://placehold.jp/332x186.png" alt="動画サンプル"/>
+                <h4 className="video-card__title">動画タイトル</h4>
+                <p className="video-card__description">概要ああああああああああああああああああ</p>
+                <button className="video-card__button">今から見る</button>
+            </div>
+            <div className="video-card">
+                <img className="video-card__thumbnail" src="http://placehold.jp/332x186.png" alt="動画サンプル"/>
+                <h4 className="video-card__title">動画タイトル</h4>
+                <p className="video-card__description">概要ああああああああああああああああああ</p>
+                <button className="video-card__button">今から見る</button>
+            </div>
+            <div className="video-card">
+                <img className="video-card__thumbnail" src="http://placehold.jp/332x186.png" alt="動画サンプル"/>
+                <h4 className="video-card__title">動画タイトル</h4>
+                <p className="video-card__description">概要ああああああああああああああああああ</p>
+                <button className="video-card__button">今から見る</button>
+            </div>
+            <div className="video-card">
+                <img className="video-card__thumbnail" src="http://placehold.jp/332x186.png" alt="動画サンプル"/>
+                <h4 className="video-card__title">動画タイトル</h4>
+                <p className="video-card__description">概要ああああああああああああああああああ</p>
+                <button className="video-card__button">今から見る</button>
+            </div>
+            <div className="video-card">
+                <img className="video-card__thumbnail" src="http://placehold.jp/332x186.png" alt="動画サンプル"/>
+                <h4 className="video-card__title">動画タイトル</h4>
+                <p className="video-card__description">概要ああああああああああああああああああ</p>
+                <button className="video-card__button">今から見る</button>
+            </div>
+            <div className="video-card">
+                <img className="video-card__thumbnail" src="http://placehold.jp/332x186.png" alt="動画サンプル"/>
+                <h4 className="video-card__title">動画タイトル</h4>
+                <p className="video-card__description">概要ああああああああああああああああああ</p>
+                <button className="video-card__button">今から見る</button>
+            </div>
+            <div className="video-card">
+                <img className="video-card__thumbnail" src="http://placehold.jp/332x186.png" alt="動画サンプル"/>
+                <h4 className="video-card__title">動画タイトル</h4>
+                <p className="video-card__description">概要ああああああああああああああああああ</p>
+                <button className="video-card__button">今から見る</button>
+            </div>
+            <div className="video-card">
+                <img className="video-card__thumbnail" src="http://placehold.jp/332x186.png" alt="動画サンプル"/>
+                <h4 className="video-card__title">動画タイトル</h4>
+                <p className="video-card__description">概要ああああああああああああああああああ</p>
+                <button className="video-card__button">今から見る</button>
+            </div>
+        </div>
+    );
+};
+
+export default VideoList;
