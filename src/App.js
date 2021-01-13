8d4910f2196911f5353a32048a5ea9acc0ddfbc5
minguu <minguu42@gmail.com>
2021-01-13 10:27:12 +0900

    React Routerでルーティングを実装

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import VideoList from "./components/VideoList";
 
 import './App.css';
 import SearchForm from "./components/SearchForm";
+import {Redirect, Route, Switch} from "react-router";
 
 const App = () => {
     return (
@@ -11,8 +12,19 @@ const App = () => {
             <NavigationDrawer/>
             <div className="main">
                 <AppBar/>
-                <SearchForm/>
-                <VideoList/>
+                <Switch>
+                    <Route exact path="/">
+                        <SearchForm/>
+                        <VideoList/>
+                    </Route>
+                    <Route path="/watch">
+                        <h2>動画再生ページ</h2>
+                    </Route>
+                    <Route path="/thank">
+                        <h2>ThankYouページ</h2>
+                    </Route>
+                    <Redirect push to="/"/>
+                </Switch>
             </div>
         </div>
     );
