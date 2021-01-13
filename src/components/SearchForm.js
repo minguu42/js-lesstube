c4193c8d1894bef0b479fbb2248ae157c34f46d1
minguu <minguu42@gmail.com>
2021-01-13 10:09:12 +0900

    SearchFormコンポーネントの作成

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,16 @@
+import {MdSearch} from 'react-icons/md';
 import './SearchForm.css';
 
 const SearchForm = () => {
     return (
-        <form>
-            色々あるよ
-        </form>
+        <div className="form__outer">
+            <form className="form">
+                <input name="keyword" className="form__filed"/>
+                <button type="submit" className="form__button">
+                    <MdSearch size="32px" />
+                </button>
+            </form>
+        </div>
     );
 };
 
