6170216da2beacc8927ea77fd4d3cc4ed3a2d539
minguu <minguu42@gmail.com>
2021-01-13 15:11:36 +0900

    YouTube APIを叩いてデータを取得

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import AppBar from './components/AppBar';
+import {useEffect} from 'react';
 import {Redirect, Route, Switch} from "react-router";
 
+import {fetchYouTubeVideos} from './api';
+
+import AppBar from './components/AppBar';
 import NavigationDrawer from "./components/NavigationDrawer";
 import VideoList from "./components/VideoList";
 import SearchForm from "./components/SearchForm";
 import VideoFlame from "./components/VideoFlame";
-
-import './App.css';
 import ThankYouPage from "./components/ThankYouPage";
 
+import './App.css';
+
 const App = () => {
+
+    useEffect(() => {
+        fetchYouTubeVideos('sample').then((data) => {
+            console.log(data.items);
+        });
+    }, []);
+
     return (
         <div className="app">
             <NavigationDrawer/>
