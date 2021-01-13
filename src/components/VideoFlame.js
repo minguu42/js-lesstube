c75e39583ffa73e5d3a780ec675acdefb4c2cb40
minguu <minguu42@gmail.com>
2021-01-13 10:48:14 +0900

    VideoFlameコンポーネントの作成

--- src/components/VideoFlame.js
+++ src/components/VideoFlame.js
@@ -4,13 +4,14 @@ const VideoFlame = () => {
     return (
         <div className="video-flame__outer">
             <iframe
-                width="560"
-                height="315"
+                width="1120px"
+                height="630px"
                 src="https://www.youtube.com/embed/MlPxDmG4VHE"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
             />
+            <h2 className="video-flame__title">動画タイトル</h2>
         </div>
     );
 };
